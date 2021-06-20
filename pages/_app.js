1e0b7b2156a9a9bc45e4a1ab9b27b2004a3c6e92
Sahil Rathee (rttss) <sahil.git@outlook.com>
2021-06-20 23:38:27 +0530

    dfsafsdf

--- pages/_app.js
+++ pages/_app.js
@@ -1,16 +1,16 @@
 // import "../styles/globals.css";
-import "../styles/dist/global.css";
-import "../styles/dist/variables.css";
-import "../styles/dist/fonts.css";
+import "../styles/global.css";
+import "../styles/variables.css";
+import "../styles/fonts.css";
 
 // CSS
-import "../styles/modules/Home.css";
-import "../styles/modules/Header.css";
-import "../styles/modules/Cart.css";
-import "../styles/modules/Footer.css";
-import "../styles/modules/Products.css";
-import "../styles/modules/Wishlist.css";
-import "../styles/modules/HomePageCategories.css";
+import "../styles/Home.css";
+import "../styles/Header.css";
+import "../styles/Cart.css";
+import "../styles/Footer.css";
+import "../styles/Products.css";
+import "../styles/Wishlist.css";
+import "../styles/HomePageCategories.css";
 
 import { Provider } from "react-redux";
 import store from "../store";
