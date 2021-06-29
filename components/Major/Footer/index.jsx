f9e3ef7fb5f902114dea3ac31d58e68ac1812a6c
Sahil Rathee (rttss) <sahil.git@outlook.com>
2021-06-29 22:51:44 +0530

    wertsaed

--- components/Major/Footer/index.jsx
+++ components/Major/Footer/index.jsx
@@ -1,24 +1,47 @@
 import React from "react";
 import { connect } from "react-redux";
 import Link from "next/link";
+import { FiShoppingCart } from "react-icons/fi";
+import { GiHamburgerMenu } from "react-icons/gi";
+import { AiFillShop } from "react-icons/ai";
+import { FaList } from "react-icons/fa";
+import { BiHomeCircle } from "react-icons/bi";
 
 export const Footer = (props) => {
+  React.useEffect(() => {
+    console.log(document.URL);
+  });
   return (
     <div className="footer">
       <Link href="/" className="footer-item">
-        Home
+        <div className="footer-item">
+          <BiHomeCircle />
+          Home
+        </div>
       </Link>
       <Link href="/category" className="footer-item">
-        Categories
+        <div className="footer-item">
+          <FaList />
+          Categories
+        </div>
       </Link>
       <Link href="/shop" className="footer-item">
-        Shop
+        <div className="footer-item">
+          <AiFillShop />
+          Shop
+        </div>
       </Link>
       <Link href="/cart" className="footer-item">
-        Cart
+        <div className="footer-item">
+          <FiShoppingCart />
+          Cart
+        </div>
       </Link>
       <Link href="/more" className="footer-item">
-        More
+        <div className="footer-item">
+          <GiHamburgerMenu />
+          More
+        </div>
       </Link>
     </div>
   );
