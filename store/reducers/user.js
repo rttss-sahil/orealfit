fa39a7dd10648ea3ecb79072c754b563794578fa
Sahil Rathee (rttss) <sahil.git@outlook.com>
2021-07-08 15:54:21 +0530

    asdfg

--- store/reducers/user.js
+++ store/reducers/user.js
@@ -1,26 +1,26 @@
 import actionTypes from "../actions/actionTypes";
 
-// const initialState = {
-//   loggedIn: false,
-//   user: {},
-// };
-
 const initialState = {
-  loggedIn: true,
-  user: {
-    email: "abc@def.com",
-    name: "undefined",
-  },
+  loggedIn: false,
+  user: {},
 };
+
+// const initialState = {
+//   loggedIn: true,
+//   user: {
+//     email: "abc@def.com",
+//     name: "undefined",
+//   },
+// };
 
 const User = (state = initialState, action) => {
   let newState;
   switch (action.type) {
     case actionTypes.addUser:
       newState = { ...action.payload, loggedIn: true };
-      // localStorage.setItem("loggedIn", true);
-      // localStorage.setItem("name", newState.user.name);
-      // localStorage.setItem("email", newState.user.email);
+      localStorage.setItem("loggedIn", true);
+      localStorage.setItem("name", newState.user.name);
+      localStorage.setItem("email", newState.user.email);
       return newState;
 
     case actionTypes.removeUser:
@@ -28,10 +28,10 @@ const User = (state = initialState, action) => {
         loggedIn: false,
         user: {},
       };
-      // localStorage.removeItem("user");
-      // localStorage.removeItem("name");
-      // localStorage.removeItem("email");
-      // localStorage.setItem("loggedIn", false);
+      localStorage.removeItem("user");
+      localStorage.removeItem("name");
+      localStorage.removeItem("email");
+      localStorage.setItem("loggedIn", false);
       return newState;
 
     default:
