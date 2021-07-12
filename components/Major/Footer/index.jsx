414744f8379b39f9251deabe41506f815fdfcbfb
Sahil Rathee (rttss) <sahil.git@outlook.com>
2021-07-13 00:56:03 +0530

    sdfdtsdfd

--- components/Major/Footer/index.jsx
+++ components/Major/Footer/index.jsx
@@ -8,15 +8,24 @@ import { FaList } from "react-icons/fa";
 import { BiHomeCircle } from "react-icons/bi";
 
 export const Footer = ({ state }) => {
+  const [url, setUrl] = React.useState("");
+  // React.useEffect(() => {
+  //   process.env.NODE_ENV === "development"
+  //     ? setUrl(document.URL.slice(22))
+  //     : setUrl(document.URL.slice(22));
+  // }, [document && document.URL]);
   return (
     <div className="footer">
       <Link href="/" className="footer-item-link">
-        <div className="footer-item">
+        <div className={`footer-item ${url === "" && " active"}`}>
           <BiHomeCircle />
           Home
         </div>
       </Link>
-      <Link href="/category" className="footer-item">
+      <Link
+        href="/category"
+        className={`footer-item ${url === "category" && " active"}`}
+      >
         <div className="footer-item">
           <FaList />
           Categories
@@ -28,14 +37,17 @@ export const Footer = ({ state }) => {
           Shop
         </div>
       </Link> */}
-      <Link href="/cart" className="footer-item">
+      <Link
+        href="/cart"
+        className={`footer-item ${url === "cart" && " active"}`}
+      >
         <div className="footer-item">
           <FiShoppingCart />
           Cart ({state.cart.length})
         </div>
       </Link>
       <Link href="/more" className="footer-item">
-        <div className="footer-item">
+        <div className={`footer-item ${url === "more" && " active"}`}>
           <GiHamburgerMenu />
           More
         </div>
--- components/Major/Homepage/index.jsx
+++ components/Major/Homepage/index.jsx
@@ -9,11 +9,18 @@ import slider1 from "../../../public/img/slider/img1.jpg";
 import slider2 from "../../../public/img/slider/img2.jpg";
 import slider3 from "../../../public/img/slider/img3.jpg";
 
+import sportSupplements from "../../../public/img/shop-by-categories/sport-supplements.webp";
+import healthyFood from "../../../public/img/shop-by-categories/healthy-food.webp";
+import vitaminAndHealth from "../../../public/img/shop-by-categories/vitamins-and-health.webp";
+import sportswearAndAccessories from "../../../public/img/shop-by-categories/sportswear-and-accessories.webp";
+
+import topSellers from "../../../public/json/top-sellers.json";
+
 export const HomePage = ({ state, dispatch }) => {
   return (
     <div className="home-page">
       <h1>HomePage</h1>
-      <div className="container">
+      <div className="slider-all">
         <AwesomeSlider>
           <div className="slider-item">
             <Image src={slider1} height="300" width="300" />
@@ -26,12 +33,51 @@ export const HomePage = ({ state, dispatch }) => {
           </div>
         </AwesomeSlider>
 
-        {/* {!state.user.loggedIn && (
+        {!state.user.loggedIn && (
           <>
             <p>Become a member of Orealfit,</p>
             <Link href="/signup">Join Us</Link>
           </>
-        )} */}
+        )}
+      </div>
+      <div className="shop-by-categories">
+        <h2>Shop by Categories</h2>
+        <div className="shop-by-categories-all">
+          <div className="shop-by-categories-item">
+            <Image src={sportSupplements} objectFit="fill" />
+            <Link href="/">Sports Supplements</Link>
+            <Link href="/">Start Shopping</Link>
+          </div>
+          <div className="shop-by-categories-item">
+            <Image src={healthyFood} objectFit="fill" />
+            <Link href="/">Healthy Food</Link>
+            <Link href="/">Start Shopping</Link>
+          </div>
+          <div className="shop-by-categories-item">
+            <Image src={vitaminAndHealth} objectFit="fill" />
+            <Link href="/">Vitamin And Health</Link>
+            <Link href="/">Start Shopping</Link>
+          </div>
+          <div className="shop-by-categories-item">
+            <Image src={sportswearAndAccessories} objectFit="fill" />
+            <Link href="/">Sports Wear And Accessories</Link>
+            <Link href="/">Start Shopping</Link>
+          </div>
+        </div>
+      </div>
+      <div className="top-sellers">
+        <h2>Top Sellers</h2>
+        <div className="top-sellers-all">
+          {topSellers.map((product) => (
+            <div className="top-sellers-item" key={product.id}>
+              <div>
+                <Image src={product.images[0].src} height={200} width={200} />
+              </div>
+              <p>{product.name}</p>
+              <div className="bottom">₹{product.price}</div>
+            </div>
+          ))}
+        </div>
       </div>
     </div>
   );
