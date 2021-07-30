06d13bf7df853b150b0ac771de0910572e78ef22
Infortts <sahil.git@outlook.com>
2021-07-31 01:57:53 +0530

    dgzdfesr

--- pages/category/index.jsx
+++ pages/category/index.jsx
@@ -1,12 +1,98 @@
 import React from "react";
 import { connect } from "react-redux";
 import Link from "next/link";
+import Image from 'next/image';
+
+import proteins from '../../public/img/shop-by-categories/normal_1460333_o.png'
+import gainers from '../../public/img/shop-by-categories/normal_1476745_o.png'
+import prepost from '../../public/img/shop-by-categories/normal_1476747_o.png'
+import fatloss from '../../public/img/shop-by-categories/normal_1476749_o.png'
+import vitamins from '../../public/img/shop-by-categories/normal_1476751_o.png'
 
 export const Category = ({ state }) => {
   return (
     <div className="category-page">
       <h1 className="category-title">All Categories</h1>
-      <ul className="category-all">
+      <div className="categories-all">
+        <div className="category">
+          <div className="category-heading">
+        <Image src={proteins} />
+        <p>Protein</p>
+          </div>
+          <ul>
+            <li><Link href="/category/">All Proteins</Link></li>
+            <li><Link href="/category/whey-proteins">Whey Protein</Link></li>
+            <li><Link href="/category/whey-protein-isolate">Whey Protein Isloate</Link></li>
+            <li><Link href="/category/whey-concentrate">Whey Protein Concentrate</Link></li>
+            <li><Link href="/category/">Reus Whey</Link></li>
+          </ul>
+        </div>
+        <div className="category">
+          <div className="category-heading">
+        <Image src={gainers} />
+<p>
+
+            Gainers
+</p>
+          </div>
+          <ul>
+            <li><Link href="/category/">All Gainers</Link></li>
+            <li><Link href="/category/weight-gain">Weight Gain</Link></li>
+            <li><Link href="/category/whey-protein-isolate">L </Link></li>
+            <li><Link href="/category/whey-concentrate">Muscle Gain</Link></li>
+          </ul>
+        </div>
+        <div className="category">
+          <div className="category-heading">
+        <Image src={prepost} />
+<p>
+  
+            Pre / Post Workout
+</p>
+          </div>
+          <ul>
+            <li><Link href="/category/">All Pre/Post Workout</Link></li>
+            <li><Link href="/category/weight-gain">Pre Workout</Link></li>
+            <li><Link href="/category/whey-protein-isolate">BCAA</Link></li>
+            <li><Link href="/category/whey-concentrate">L Arginine</Link></li>
+            <li><Link href="/category/whey-concentrate">EAA</Link></li>
+            <li><Link href="/category/whey-concentrate">Creatine</Link></li>
+            <li><Link href="/category/whey-concentrate">Glutamine</Link></li>
+          </ul>
+        </div>
+        <div className="category">
+          <div className="category-heading">
+        <Image src={fatloss} />
+<p>
+
+            Fat Loss
+</p>
+          </div>
+          <ul>
+            <li><Link href="/category/">All Fat Loss</Link></li>
+            <li><Link href="/category/weight-gain">CLA</Link></li>
+            <li><Link href="/category/whey-protein-isolate">L Arginine</Link></li>
+            <li><Link href="/category/whey-concentrate">L Cosmitine</Link></li>
+            <li><Link href="/category/whey-concentrate">Garcinia Com</Link></li>
+          </ul>
+        </div>
+        <div className="category">
+          <div className="category-heading">
+        <Image src={vitamins} />
+<p>
+
+            Vitamins & Wellness
+</p>
+          </div>
+          <ul>
+            <li><Link href="/category/">All Vitamins & Wellness</Link></li>
+            <li><Link href="/category/weight-gain">Omega Fatty Acids</Link></li>
+            <li><Link href="/category/whey-protein-isolate">Multi-Vitamins</Link></li>
+            <li><Link href="/category/whey-concentrate">WorkOut Essential</Link></li>
+          </ul>
+        </div>
+      </div>
+      {/* <ul className="category-all">
         {[...state.categories].map((category) => (
           <Link
             className="category-item"
@@ -16,7 +102,7 @@ export const Category = ({ state }) => {
             {category.name}
           </Link>
         ))}
-      </ul>
+      </ul> */}
     </div>
   );
 };
